a487282a88ef6ba34de9ded9f887f5c90e6ca232
David Augusto <davidribeiro086@gmail.com>
2022-03-20 17:03:06 -0300

    heroku setup

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,7 +1,21 @@
 import { Request, Response } from 'express';
-
+import { pet } from '../models/pet';
+import { createMenuObject } from './helpers/createMenuObjetcts'; 
 
 export const search = (req: Request, res: Response) => {
+    let query: string = req.query.q as string;
+
+    let list = pet.getFromName(query);
     
+    if(!query) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    })
 };
 
